53a3994dc31037353d052a800fb594b6b713ad8f
Nega0619 <80737049+Nega0619@users.noreply.github.com>
2021-05-03 11:52:54 +0900

    let make simple csv file
    주식 기본 정보를 가져와서(한줄) csv파일만드는 코드  추가

--- 32bit/CommConnect.py
+++ 32bit/CommConnect.py
@@ -1,4 +1,5 @@
 from pykiwoom.kiwoom import *
+import pandas as pd
 
 kiwoom = Kiwoom()
 kiwoom.CommConnect(block=True)
@@ -6,3 +7,9 @@ print("블록킹 로그인 완료")
 
 name = kiwoom.GetMasterCodeName("005930")
 print(name)
+
+df  = kiwoom.block_request("opt10001",
+                          종목코드="005930",
+                          output="주식기본정보",
+                          next=0)
+df.to_csv('SamsungElectronics_StockBasicInformation.csv', index=False, encoding='cp949')
